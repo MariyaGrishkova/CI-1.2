2a3b73695ab4ac90e0d5372069a8c4477b12261d
Mariya Grishkova <galeyevamv@gmail.com>
2021-03-19 19:50:37 +0300

    .gitignore edited

--- src/test/java/ru/netology/rest/MobileBankApiTestV2.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV2.java
@@ -1,6 +1,7 @@
 package ru.netology.rest;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -10,7 +11,8 @@ class MobileBankApiTestV2 {
     void shouldReturnDemoAccounts() {
         // Given - When - Then
         // Предусловия
-        given()
+        ValidatableResponse validatableResponse;
+        validatableResponse = given()
                 .baseUri("http://localhost:9999/api/v1")
                 // Выполняемые действия
                 .when()
